da0d28c647faa482c129d049de342d0b201c20ab
skyno001 <103692371+skyno001@users.noreply.github.com>
2025-12-11 17:29:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 sed -i 's/openwrt-23.05/master/g' feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld;master' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
