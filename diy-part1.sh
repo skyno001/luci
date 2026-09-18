60210d9bf767e03cdeba7610182d0dd2367f107b
skyno001 <103692371+skyno001@users.noreply.github.com>
2026-09-18 18:50:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/openwrt-25.12/openwrt-23.05/g' feeds.conf.default
+sed -i 's/openwrt-25.12/openwrt-23.05/g' feeds.conf.default
 sed -i 's/src-git helloworld/#src-git helloworld/g' feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld;master' >>feeds.conf.default
