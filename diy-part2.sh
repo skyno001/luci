089b1d57a375893bb2f9e8dd40c41cc31af0763c
skyno001 <103692371+skyno001@users.noreply.github.com>
2026-09-26 17:21:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,6 +17,9 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's#"admin/status/bandwidth"#"admin/status/realtime/bandwidth"#g' feeds/luci/modules/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
 
+sed -i 's/KERNEL_PATCHVER:=6.18/KERNEL_PATCHVER:=6.12/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=6.12/KERNEL_TESTING_PATCHVER:=6.18/g' target/linux/x86/Makefile
+
 # Modify firewall
 sed -i '118,131s/^/#/' package/network/config/firewall/files/firewall.config
 
